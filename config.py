8a9ebfa4f6b8bff5e59b0d74500900ee76f59e08
hlibvel <hlib.velyhorskyi@gmail.com>
2018-07-06 17:38:56 +0300

    fix app secret eeror

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ HERE = os.path.abspath(os.path.dirname(__file__))  # the path to the project dir
 dotenv.load_dotenv(dotenv_path=f'{HERE}/.env')     # load variables from .env file
 
 
-APP_SECRET = os.environ['APP_SECRET'].encode()
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
 
