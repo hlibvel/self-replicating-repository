b63eb8354dae9c869f61154f7576463467309f74
hlibvel <hlib.velyhorskyi@gmail.com>
2018-07-06 17:56:08 +0300

    implement repo fork

--- app.py
+++ app.py
@@ -15,14 +15,35 @@ blueprint = make_github_blueprint(
 )
 app.register_blueprint(blueprint, url_prefix="/login")
 
+
+def get_login():
+	"""Reads GitHub user name"""
+	resp = github.get("/user")
+    assert resp.ok
+    return resp.json()["login"]
+
+
+def make_a_fork(owner, repo_name):
+	"""Forks requested repository"""
+    resp = github.post(f'/repos/{owner}/{repo_name}/forks')
+    assert resp.ok
+
+    return resp.json()
+
+
 @app.route("/")
 def index():
     if not github.authorized:
         return redirect(url_for("github.login"))
 
-    resp = github.get("/user")  # read GitHub user name
-    assert resp.ok
-    return "You are @{login} on GitHub".format(login=resp.json()["login"])
+    make_a_fork(OWNER, REPO_NAME)
+    return """
+        Dear @{login},
+        now you have a fork of {owner}/{repo_name} on GitHub""".format(
+            login=get_login(),
+            owner=OWNER,
+            repo_name=REPO_NAME
+            )
 
 if __name__ == "__main__":
     app.run()
--- config.py
+++ config.py
@@ -10,3 +10,6 @@ dotenv.load_dotenv(dotenv_path=f'{HERE}/.env')     # load variables from .env fi
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
 
+OWNER = os.environ.get('OWNER', 'hlibvel')
+REPO_NAME = os.environ.get('REPO_NAME', 'self-replicating-repository')
+
